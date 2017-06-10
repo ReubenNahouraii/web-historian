49b26265cb386d68d92a383852e2f229598f59b1
Hack Reactor Student <student@hackreactor.com>
2017-06-09 20:53:26 -0700

    Implement save data, no callbacks

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,53 +6,39 @@ var archive = require('../helpers/archive-helpers');
 var serveFile = function(request, response, path = '/public/index.html') {
   var filePath = __dirname + path;
   fs.stat(filePath, function(err, stats) {
-    console.log('error', err);
     if (stats.isFile()) {
       response.writeHead(200, {
         'Content-Type': 'text/html',
-        'Content-Disposition': 'inline'});
-      let readStream = fs.createReadStream(filePath);
-      readStream.on('error', (err) => {
-        response.end(err);
-      });
-      
-      readStream.pipe(response);
-      //response.end();
-      
+        'Content-Disposition': 'inline; filename=' + 'loading.html'});
+      fs.createReadStream(filePath).pipe(response);
     } else {
       response.writeHead(400, {'Content-Type': 'text/plain'});
-      response.end('ERROR File does NOT Exist');
+      response.end('ERROR File does NOT Exists');
     }
   });  
 };
 
 exports.handleRequest = function (request, response) {
+  console.log('we are in handlerequest')
   request.setEncoding('utf8');
   
   
   if (request.method === 'POST') {
     request.on('data', function(url) {
+      console.log('we are in data')
       url = url.slice(4);
       
       // if (isUrlArchived(data)) {
       //   serveFile(downloadUrls(data), request, response);
       // }
       
-      archive.isUrlInList(url, (bool)=>{
-        // if (!bool) {
-        //   archive.addUrlToList(url);
-        // } 
-        serveFile(request, response, '/public/loading.html'); 
-      });
-      
-      
-      //  {
-      //   serveFile('/public/loading.html', request, response);  
-      // }
+      if (archive.isUrlInList(url)) {
+        serveFile('/public/loading.html', request, response);  
+      }
       
       //Assuming it doesn't exist, add the link to sites.txt
-      
-      
+      archive.addUrlToList(url);
+      serveFile(request, response, '/public/loading.html'); 
       
     });
   }
